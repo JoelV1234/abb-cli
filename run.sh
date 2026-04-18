cdd0655943469ef99125b88e652239a8ff7af03a
BaptistHub <anonlayer0@gmail.com>
2026-04-18 13:50:17 -0700

    removed run

--- run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Get the directory where this script is located
-DIR="$(cd "$(dirname "$0")" && pwd)"
-
-# Run the python script using the venv python binary directly
-"$DIR/.venv/bin/python3" "$DIR/main.py"
